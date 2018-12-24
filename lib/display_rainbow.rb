397ec064c8dacc26a02e805dcf28f440e2c0f9d8
Tyler Seacrist <seacristt@hotmail.com>
2018-12-24 00:22:16 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
- puts "R": {[0]}, "O": {[1]}, "Y": {[2]}, "G": {[3]}, "B": {[4]}, "I": {[5]}, "V": {[6]}
+ puts "R": {[0]} => "O": {[1]} => "Y": {[2]} => "G": {[3]} => "B": {[4]} => "I": {[5]} => "V": {[6]}
 end
 
